f1f7d6fb9bf3ce9a3f341a5609eb24eda43c39c1
John Claro <jkrclaro@gmail.com>
2016-05-18 01:50:10 +0100

    Use Roboto font and document No such module Alamofire error

--- Chooonz/ChooonzTableViewController.swift
+++ Chooonz/ChooonzTableViewController.swift
@@ -7,13 +7,17 @@
 //
 
 import UIKit
+import Alamofire
+import AlamofireImage
 
 class ChooonzTableViewController: UITableViewController, UISearchResultsUpdating {
+    let YOUTUBE_API_KEY = "AIzaSyASQmuEQjisq2etMyH20mfd84OQJIU_2KE"
     
     var songTitles = ["Such Dumb Luck", "Shake Shake", "I Was Born", "Nothing Lasts Forever", "Tempting Me"]
     var songArtists = ["The Pale", "REWS", "Otherkin", "MKAI", "In The Willows"]
     var songArtistsBio = ["The Pale started life in Dublin's Northside in the early 90's but drew their influences from far beyond. For some they are best remembered for their hits 'Butterfly' and 'Dogs with no tails'. But that is only the tip of the iceberg for a band that has steered clear of the beaten track.", "REWS is the Irish/ London based high energy female duo consisting of songstress Shauna Tohill and beat-maker Collette Williams who together are creating a genuine buzz with their brand of ultra catchy high energy alt rock.", "Dublin grunge-pop outfit Otherkin keep the raucous rock vibes going in a world of soulless synth pop.", "All adept players from an early age, MKAI incorporate wandering guitar figure, piano and electronics into songs replete with a strong sense of melody and urgency.", "Formed in 2011 as an acoustic trio, Waterford band In The Willows gained speedy recognition. The band has now evolved to a sextet, still mixing it up across the genres! In The Willows’ debut album saw the group mixing the lines between folk, alternative, pop and rock to create their own heartwarming and aurally fulfilling sound."]
     var songImages = ["the_pale", "rews", "otherkin", "mkai", "in_the_willows"]
+    var songYouTubeIDs = ["lGsawtEQJos", "QTAx0uUpJz0", "bTkUBgQummw", "5E5Pquvx5hA", "aI7XUBExCyI"]
     
     var filteredSongs = [String]()
     var searchController: UISearchController!
@@ -58,8 +62,7 @@ class ChooonzTableViewController: UITableViewController, UISearchResultsUpdating
         if self.searchController.active {
             cell.songTitle?.text = self.filteredSongs[indexPath.row]
             cell.songArtist?.text = self.filteredSongs[indexPath.row]
-            
-            
+        
             cell.songArtistImage?.image = UIImage(named: self.songImages[indexPath.row])
             // Give image rounded corners
             cell.songArtistImage.layer.cornerRadius = cell.songArtistImage.frame.size.width / 2
@@ -68,6 +71,20 @@ class ChooonzTableViewController: UITableViewController, UISearchResultsUpdating
         } else {
             cell.songTitle?.text = self.songTitles[indexPath.row]
             cell.songArtist?.text = self.songArtists[indexPath.row]
+            
+            let songThumbnailURL = "http://i1.ytimg.com/vi/" + self.songYouTubeIDs[indexPath.row] + "/mqdefault.jpg"
+            Alamofire.request(.GET, songThumbnailURL)
+                .responseImage { response in
+                    debugPrint(response)
+                    print(response.request)
+                    print(response.response)
+                    debug(response.result)
+                    
+                    if let image = response.result.value {
+                        cell.songImage?.image = image
+                    }
+            }
+            
             
             cell.songArtistImage?.image = UIImage(named: self.songImages[indexPath.row])
             // Give image rounded corners
--- README.md
+++ README.md
@@ -3,7 +3,7 @@ iOS application for Chooonz using the Swift programming language. This
 application is the interface that allows users to request song details from
 a web server created in Swift.
 
-# Features
+# To-Dos
 - [x] Add icons (from logomakr.com)
 - [x] Add launch image (from logomakr.com)
 - [x] Allow user to search for song titles
@@ -12,7 +12,7 @@ a web server created in Swift.
 - [ ] Send API requests using Alamofire
 - [ ] Display YouTube thumbnail
 - [ ] Play YouTube video
-- [ ] Use Roboto font
+- [x] Use Roboto font
 - [ ] Better string filter by taking in to account the position of letters
 - [ ] Display user's recent searches
 
@@ -55,3 +55,7 @@ To implement our new pods, run the following commands.
 $ cd Users/johnclaro/<path_to_Chooonz>
 $ pod install
 ```
+
+# Known Errors
+> If a red warning saying "No such module 'Alamofire'" is showing up. Go to
+the directory of the repository and open up the Chooonz.xcworkspace
